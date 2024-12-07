813846af785072a10ee7217aa66a41aceecfbbb7
dselva-2005 <dselvajagan@gmail.com>
2024-12-07 19:05:27 +0530

    gsc verification added

--- shop/urls.py
+++ shop/urls.py
@@ -6,7 +6,7 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.home, name='home'),
     path('shop/', views.product_list, name='product_list'),
-    path('googleb0dbb8bde61b8b0b/', views.verify, name='gsc_verification'),
+    path('googleb0dbb8bde61b8b0b.html/', views.verify, name='gsc_verification'),
     path('<int:id>/<slug:slug>/', views.product_detail,name='product_detail'),
     path('<slug:category_slug>/', views.product_list,name='product_list_by_category'),
 ]
