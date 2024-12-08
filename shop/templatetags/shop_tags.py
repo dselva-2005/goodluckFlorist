8d2c207e0db7000b0d3d3d5fc5142147393fb2af
dselva-2005 <dselvajagan@gmail.com>
2024-12-08 14:03:21 +0530

    ui changes

---
+++ shop/templatetags/shop_tags.py
@@ -0,0 +1,8 @@
+from django import template
+from shop.models import Product
+register = template.Library()
+
+@register.simple_tag
+def getSingleImg(product_id):
+    product = Product.objects.get(id=product_id)
+    return product.images.first().image.url
